d7525baa0a97e3a8c79ee7e0200aa3027882c543
Florian Jourde <jourdeflorian@gmail.com>
2024-02-28 19:34:21 +0100

    Update routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,26 @@
 import Navbar from "./components/Navbar";
-import {BrowserRouter, Switch, Route} from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Destination from "./pages/Destination";
 import Crew from "./pages/Crew";
 import Technology from "./pages/Technology";
 import "./styles/index.scss";
+import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Navbar/>
+        <Navbar />
+
         <Switch>
-          <Route exact path="/">
-            <Home/>
-          </Route>
-          <Route path="/destination">
-            <Destination/>
-          </Route>
-          <Route path="/crew">
-            <Crew/>
-          </Route>
-          <Route path="/technology">
-            <Technology/>
-          </Route>
+          <Route exact path="/" component={Home} />
+          <Route path="/destination" component={Destination} />
+          <Route path="/crew" component={Crew} />
+          <Route path="/technology" component={Technology} />
+          <Redirect to="/" />
         </Switch>
+
       </BrowserRouter>
     </div>
   )
