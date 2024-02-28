8f83eee6e0055eaf6d30a5158dea05aa3d5a1b73
Florian Jourde <jourdeflorian@gmail.com>
2024-02-28 20:02:24 +0100

    Update roots

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -12,7 +12,7 @@ function Navbar({ baseUrl }) {
 
   return (
     <nav className={'main-navbar'}>
-      <Link to={baseUrl + "/"} className={'navbar-logo'}>
+      <Link to={baseUrl} className={'navbar-logo'}>
         <img src={logo} alt="" />
       </Link>
       <button className={'toggle-menu open'} onClick={handleToggleMenu}>
@@ -24,7 +24,7 @@ function Navbar({ baseUrl }) {
         </button>
         <ul>
           <li><NavLink className={({ isActive }) => (isActive && 'active')} exact to={baseUrl}><span className={'number'}>00</span> Home</NavLink></li>
-          <li><NavLink className={({ isActive }) => (isActive && 'active')} to={baseUrl + "destination"}><span className={'number'}>01</span> Destination</NavLink></li>
+          <li><NavLink className={({ isActive }) => (isActive && 'active')} to={baseUrl + "/destination"}><span className={'number'}>01</span> Destination</NavLink></li>
           <li><NavLink className={({ isActive }) => (isActive && 'active')} to={baseUrl + "crew"}><span className={'number'}>02</span> Crew</NavLink></li>
           <li><NavLink className={({ isActive }) => (isActive && 'active')} to={baseUrl + "technology"}><span className={'number'}>03</span> Technology</NavLink></li>
         </ul>
