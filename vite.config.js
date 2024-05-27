23ca50ba51fbe42222cb729384aa5985683e4657
Florian Jourde <f.jourde@facil-iti.com>
2024-05-27 16:29:12 +0200

    Enable displayName

--- vite.config.js
+++ vite.config.js
@@ -4,19 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(
-    // {
-    //   babel: {
-    //     plugins: [
-    //       [
-    //         'babel-plugin-styled-components',
-    //         {
-    //           displayName: true,
-    //           fileName: true
-    //         }
-    //       ]
-    //     ]
-    //   }
-    // }
+    {
+      babel: {
+        plugins: [
+          [
+            'babel-plugin-styled-components',
+            {
+              displayName: true,
+              fileName: true
+            }
+          ]
+        ]
+      }
+    }
   )],
   base: '/Frontend-Mentor-20-Space-tourism-multi-page-website/'
 })
