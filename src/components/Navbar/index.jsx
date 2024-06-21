e187a1ed62250e34821b08142f312c6d9a570fe3
Florian Jourde <f.jourde@facil-iti.com>
2024-06-21 15:29:58 +0200

    Experiments w/ CSS modules

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,5 +1,6 @@
 import { Link, NavLink } from "react-router-dom";
 import '../../styles/Navbar.scss';
+import './styles.module.css';
 import logo from '../../assets/shared/logo.svg';
 import burgerIcon from '../../assets/shared/icon-hamburger.svg';
 import closeIcon from '../../assets/shared/icon-close.svg';
@@ -25,10 +26,18 @@ function Navbar({ baseUrl }) {
           <img src={closeIcon} alt="" />
         </button>
         <ul>
-          <li><NavLink className={({ isActive }) => (isActive && 'active')} exact to={baseUrl}><span className={'number'}>00</span> Home</NavLink></li>
-          <li><NavLink className={({ isActive }) => (isActive && 'active')} to={baseUrl + "destination"}><span className={'number'}>01</span> Destination</NavLink></li>
-          <li><NavLink className={({ isActive }) => (isActive && 'active')} to={baseUrl + "crew"}><span className={'number'}>02</span> Crew</NavLink></li>
-          <li><NavLink className={({ isActive }) => (isActive && 'active')} to={baseUrl + "technology"}><span className={'number'}>03</span> Technology</NavLink></li>
+          <li>
+            <NavLink className={({ isActive }) => (isActive && 'active')} exact to={baseUrl}><span className={'number'}>00</span> Home</NavLink>
+            </li>
+          <li>
+            <NavLink className={({ isActive }) => (isActive && 'active')} to={baseUrl + "destination"}><span className={'number'}>01</span> Destination</NavLink>
+            </li>
+          <li>
+            <NavLink className={({ isActive }) => (isActive && 'active')} to={baseUrl + "crew"}><span className={'number'}>02</span> Crew</NavLink>
+            </li>
+          <li>
+            <NavLink className={({ isActive }) => (isActive && 'active')} to={baseUrl + "technology"}><span className={'number'}>03</span> Technology</NavLink>
+            </li>
         </ul>
       </div>
     </nav>
